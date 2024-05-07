676c64de8eb7e64be35d098ca4a21ca66161cfc6
MarcoAbraham <168035049+MarcoAbraham@users.noreply.github.com>
2024-05-07 02:16:57 -0600

    App

---
+++ _app/immutable/nodes/5.ADmfW-fr.js
@@ -0,0 +1,64 @@
+import{s as c,n as a}from"../chunks/scheduler.xiAKmmds.js";import{S as t,i,e as m,c as r,g as u,a as d,b as p,d as v}from"../chunks/index.9LEsI8pP.js";const g=""+new URL("../assets/enlace.CCN-NwEh.jpg",import.meta.url).href,b=""+new URL("../assets/enlace2.DE4O_Ynt.jpg",import.meta.url).href,f=""+new URL("../assets/enlaceionico.BTzX73Lw.jpg",import.meta.url).href,q=""+new URL("../assets/enlacecovalente.BDn285T2.webp",import.meta.url).href,E=""+new URL("../assets/enlacemetalico.DuUCgU_0.png",import.meta.url).href;function y(l){let e,o=`<h1 class="svelte-iemmg5">Enlaces Químicos</h1> <details class="svelte-iemmg5"><summary class="svelte-iemmg5">Haz click aqui para un resumen con IA</summary> <p class="svelte-iemmg5">Los enlaces químicos unen átomos en moléculas o compuestos. Se
+			forman al compartir o transferir electrones. Los tipos principales
+			son iónicos (transferencia), covalentes (compartición) y metálicos
+			(liberación). Aunque los átomos buscan ocho electrones en su capa de
+			valencia (regla del octeto), hay excepciones.</p></details> <details class="svelte-iemmg5"><summary style="color: red;" class="svelte-iemmg5">Haz click aqui para ver un examen</summary> <p class="svelte-iemmg5"><b>1. ¿Qué son los enlaces químicos?</b><br/>
+			Fuerzas que mantienen unidos a los átomos en una molécula o compuesto.<br/> <b>2. ¿Cuál es el tipo de enlace que se forma por la transferencia
+				de electrones entre átomos?</b><br/>
+			Enlace iónico<br/> <b>3. ¿Qué tipo de enlace se caracteriza por la compartición de
+				electrones entre átomos?</b><br/>
+			Enlace covalente<br/> <b>4. ¿Cuál es la regla que establece que los átomos buscan tener
+				8 electrones en su capa de valencia?</b><br/>
+			Regla del octeto<br/> <b>5. ¿Los metales son buenos conductores de electricidad debido a
+				qué tipo de enlace?</b><br/>
+			Enlace metálico<br/></p></details> <p class="svelte-iemmg5">Los enlaces químicos son las fuerzas que mantienen unidos a los átomos
+		en una molécula o un compuesto. <br/>Estos enlaces se forman a través
+		de la compartición o la transferencia de electrones entre los átomos, lo
+		que determina las propiedades químicas de las sustancias.</p> <br/> <p class="svelte-iemmg5">Existen diferentes tipos de enlaces químicos, que se clasifican en
+		enlaces iónicos, covalentes y metálicos. Los enlaces iónicos se forman
+		por la transferencia de electrones entre átomos de diferentes elementos,
+		mientras que los enlaces covalentes se producen por la compartición de
+		electrones entre átomos de la misma especie. Por último, los enlaces
+		metálicos se caracterizan por la liberación de electrones de valencia de
+		los átomos.</p> <br/> <p class="svelte-iemmg5">Las aplicaciones de los enlaces químicos son fundamentales en numerosos
+		campos de la química, como en la síntesis de compuestos, el diseño de
+		nuevos materiales y en la comprensión de las reacciones químicas. Sin
+		embargo, la regla del octeto, que establece que los átomos tienden a
+		completar su capa de valencia con ocho electrones, presenta algunas
+		limitaciones en átomos que pueden tener más de ocho electrones en su
+		capa externa, como los elementos de la tercera fila de la tabla
+		periódica.</p> <div class="image svelte-iemmg5"><img src="${b}" alt="Enlaces Químicos" class="svelte-iemmg5"/> <p class="svelte-iemmg5">Enlace 2</p></div> <br/> <p class="svelte-iemmg5">En resumen, los enlaces químicos son las fuerzas que mantienen unidos a
+		los átomos en una molécula o un compuesto, se clasifican en enlaces
+		iónicos, covalentes y metálicos, y tienen aplicaciones en diversos
+		campos de la química, aunque la regla del octeto presenta algunas
+		limitaciones en determinadas situaciones.</p> <div class="image svelte-iemmg5"><img src="${g}" alt="Enlaces Químicos" class="svelte-iemmg5"/> <p class="svelte-iemmg5">Esta es una imagen de un enlace</p></div> <br/> <p class="svelte-iemmg5">Los enlaces químicos son la fuerza que mantiene unidos a los átomos en
+		una molécula. Son indispensables para la formación de compuestos
+		químicos y determinan las propiedades de las sustancias. En este ensayo,
+		se explorará el concepto de enlace químico, su clasificación y las
+		aplicaciones y limitaciones de la regla de octeto.</p> <p class="svelte-iemmg5">El enlace químico se define como la fuerza de atracción que mantiene
+		unidos a los átomos en una molécula. Los atomos se unen entre sí para
+		cumplir con la regla del octeto, es decir, tener ocho electrones en su
+		capa de valencia. Existen distintos tipos de enlaces químicos, como el
+		enlace iónico, covalente y metálico.</p> <br/> <p class="svelte-iemmg5">El enlace iónico se forma cuando dos átomos con electronegatividades muy
+		diferentes se unen, y uno cede electrones al otro. Por ejemplo, en la
+		formación del cloruro de sodio (NaCl), el sodio cede un electrón al
+		cloro, formando un enlace iónico. En este tipo de enlace, los átomos
+		adquieren carga eléctrica y se unen a través de atracción
+		electrostática.</p> <div class="image svelte-iemmg5"><img src="${f}" alt="Enlaces Químicos" class="svelte-iemmg5"/> <p class="svelte-iemmg5">Enlace iónico</p></div> <br/> <p class="svelte-iemmg5">Por otro lado, el enlace covalente se forma cuando dos átomos comparten
+		pares de electrones para cumplir con la regla del octeto. Este tipo de
+		enlace es más fuerte que el iónico y se presenta en moléculas de
+		elementos no metálicos, como el oxígeno en el agua (H2O). Los electrones
+		compartidos se encuentran en una región de alta densidad electrónica
+		llamada enlace covalente.</p> <div class="image svelte-iemmg5"><img src="${q}" alt="Enlaces Químicos" class="svelte-iemmg5"/> <p class="svelte-iemmg5">Enlace covalente</p></div> <br/> <p class="svelte-iemmg5">Por último, el enlace metálico se forma en los metales, donde los
+		electrones se mueven libremente en una nube electrónica compartida. Esta
+		característica permite que los metales sean buenos conductores de
+		electricidad y calor, ya que los electrones pueden moverse con facilidad
+		a través del material.</p> <br/> <div class="image svelte-iemmg5"><img src="${E}" alt="Enlaces Químicos" class="svelte-iemmg5"/> <p class="svelte-iemmg5">Enlace metálico</p></div> <p class="svelte-iemmg5">En cuanto a las aplicaciones y limitaciones de la regla del octeto, este
+		principio se usa comúnmente para determinar la estabilidad de los
+		compuestos químicos. Sin embargo, existen algunas excepciones a esta
+		regla, como en el caso de los compuestos con menos de ocho electrones en
+		su capa de valencia, como el tritio (H3) o el monóxido de carbono (CO).</p> <br/> <p class="svelte-iemmg5">En conclusión, los enlaces químicos son fundamentales para la formación
+		de compuestos químicos y determinan las propiedades de las sustancias. A
+		través de la clasificación de los enlaces químicos y el estudio de la
+		regla del octeto, es posible comprender mejor la naturaleza de las
+		interacciones entre átomos y moléculas en la química.</p>`;return{c(){e=m("div"),e.innerHTML=o,this.h()},l(s){e=r(s,"DIV",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-1xelz9j"&&(e.innerHTML=o),this.h()},h(){d(e,"class","content svelte-iemmg5")},m(s,n){p(s,e,n)},p:a,i:a,o:a,d(s){s&&v(e)}}}class x extends t{constructor(e){super(),i(this,e,null,y,c,{})}}export{x as component};
